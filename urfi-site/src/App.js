a8f687d9e560c6551b583eb6e22d378858a49d52
Brian Tufts <bt3qzd@virginia.edu>
2020-08-13 15:44:21 -0400

    files

--- urfi-site/src/App.js
+++ urfi-site/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Homepage from "./site/Homepage.js";
+import Checkspage from "./site/ChecksPage.js";
+import Web from "./site/Web.js";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/site/Homepage.js">Homepage</Link>
+            </li>
+            <li>
+              <Link to="/site/CheckPage.js">About</Link>
+            </li>
+            <li>
+              <Link to="/site/Web.js">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/site/ChecksPage.js">
+            <Checkspage />
+          </Route>
+          <Route path="/site/Web.js">
+            <Web />
+          </Route>
+          <Route path="/site/Homepage.js">
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ urfi-site/src/site/ChecksPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Homepage.css";
+
+class ChecksPage extends Component {
+    render() {
+        return (
+            <h2>ChecksPage</h2>
+        );
+    }
+}
+
+export default ChecksPage;
